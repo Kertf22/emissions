869dc33cdef2cbdfb142ad5189dcd34dae59558b
Thiago Freire de Carvalho <tfcarvalho@sedurbs.se>
2023-07-20 17:46:18 -0300

    url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://emissions-api.onrender.com/",
-    baseURL: "http://localhost:3000/",
+    baseURL: "https://emissions-api.onrender.com/",
 });
 
 export default api;
